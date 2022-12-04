537b86be335376c240f1a0c82f1c6885fc4de6e9
karandin <misakarandin@gmail.com>
2022-12-04 16:02:01 +0300

    1.0.1 Delete language section add section "about"

--- src/main/java/me/telegram/bot/Main.java
+++ src/main/java/me/telegram/bot/Main.java
@@ -7,7 +7,7 @@ package me.telegram.bot;
 
 public class Main {
 
-    private static final String BOT_TOKEN = "5886775487:AAHQeOuJz37iYtA51d0Th-DsOi1egREoYYw";
+    private static final String BOT_TOKEN = "5615013459:AAEcYNmeT18iRre3Y34za65cZLnC84Rn6Ao";
 
     private static final String PROVIDER_TOKEN = "1877036958:TEST:533be1fb7c0abd266c7c8d87ff053c592db708c9";
 
